489039d43a8bad5c2091c05bdfce4df49912b7c8
Andre Madarang <andre_madarang@hotmail.com>
2019-07-16 16:02:43 -0400

    another change

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -1,4 +1,31 @@
-@servers(['web-1' => 'forge@andredemos.ca', 'web-2' => 'forge@laravelecommerceexample.ca'])
+@servers(['web-1' => 'forge@andredemos.ca'])
+
+@task('git')
+    cd /home/forge/envoy.andredemos.ca
+    git pull origin master
+@endtask
+
+@task('composer')
+    cd /home/forge/envoy.andredemos.ca
+    composer install --no-interaction --prefer-dist --optimize-autoloader
+@endtask
+
+@task('migrate')
+    cd /home/forge/envoy.andredemos.ca
+    if [ -f artisan ]
+    then
+        php artisan migrate --force
+    fi
+@endtask
+
+@story('deploy')
+    git
+    composer
+    migrate
+@endstory
+
+
+{{-- @servers(['web-1' => 'forge@andredemos.ca', 'web-2' => 'forge@laravelecommerceexample.ca'])
 
 @task('list', ['on' => 'web-1', 'confirm' => true])
     cd envoy.andredemos.ca
@@ -13,4 +40,4 @@
 @story('awesome')
     list
     inspire
-@endstory
+@endstory --}}
